d79bfc9e912b89321a5e1576d6b63150810cb366
Antonin <antonin.maubert@edu.esiee-it.fr>
2025-04-08 13:55:23 +0200

    good version

--- resources/views/components/main/sidebar.blade.php
+++ resources/views/components/main/sidebar.blade.php
@@ -215,7 +215,7 @@
                                 @csrf
                                 <a class="btn btn-sm btn-light justify-center"
                                    href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();">
-                                    {{ (--'Log out') }}
+                                    {{ ('Log out') }}
                                 </a>
                             </form>
                         </div>
